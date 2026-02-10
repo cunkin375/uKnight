afebe0c17bf48e45bcd83f6594266898b2cb7871
Ali <jeremywr22@gmail.com>
2026-02-09 20:49:12 -0500

    update dependencies to include firebase

--- src/main/java/com/uknight/videocall/VideocallApplication.java
+++ src/main/java/com/uknight/videocall/VideocallApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VideocallApplication {
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(VideocallApplication.class, args);
 	}
 
